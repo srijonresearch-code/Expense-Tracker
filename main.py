584313062d7b0c8d9e18f5e54a65934bb0f49c9c
Srijon Das <srijon.research@gmail.com>
2026-03-07 15:24:34 +0600

    main.py file updated

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-print("Expense Tracker")
-print("1. Add Expense")
-print("2. View Expenses")
-print("3. View Total Spent")
-print("4. Exit")
-option=int(input("Choose Option: "))
+expences=[]
+while True:
+    print("Expense Tracker")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. View Total Spent")
+    print("4. Exit")
+    option=int(input("Choose Option: "))
 
-if option==1:
-    amount=int(input("Enter amount: "))
-    catagory=(input("Enter category: "))
-    date=input("Enter date: ")
-    
+    if option==1:
+        amount=int(input("Enter amount: "))
+        catagory=input("Enter category: ")
+        date=input("Enter date: ")
+        expences.append({"Amount":amount,"Catagory":catagory,"Date":date})
+    elif option==2:
+        print(expences)
+    elif option==4:
+        print("Program exited successfully!")
+        break
+    else:
+        print("Enter correct option: ")
