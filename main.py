bb54ad0e59d4f852716deda5060ca6cebc5f4d56
Srijon Das <srijon.research@gmail.com>
2026-03-10 18:57:36 +0600

    Updated main.py and added expences.json file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import json
+with open("expences.json","r") as file:
+    expences=json.load(file)
+print(expences)
 print("-"*5,"Expense Tracker","-"*5)
-expences=[]
+expence=[]
 while True:
     print("1. Add Expense")
     print("2. View Expenses")
@@ -12,18 +16,27 @@ while True:
         catagory=input("Enter category: ")
         amount=int(input("Enter amount: "))
         date=input("Enter date: ")
-        expences.append({"Catagory":catagory,"Amount":amount,"Date":date})
+        expence.append({"Catagory":catagory,"Amount":amount,"Date":date})
         print("Expense added successfully!")
         print("-"*20)
     elif option==2:
-        for el in expences:
+        for el in expence:
             for key,value in el.items():
                 print(f"{key}:{value}")
             print("-"*20)
     elif option==3:
-        pass
+        for element in expence:
+            for key,value in element.items():
+                if value==amount:    
+                    total_spent=0
+                    for amount_value in element.items():
+                        total_spent+=amount
+        print("Total spent: ",total_spent)
+        print("-"*20)
     elif option==4:
         print("Program exited successfully!")
+        print("-"*20)
         break
     else:
         print("Enter correct option.")
+        print("-"*20)
