185ffecf58d8746bb077f40f7b7068c9fab24987
Srijon Das <srijon.research@gmail.com>
2026-03-11 13:02:05 +0600

    updated main.py and renamed expenses.json

--- main.py
+++ main.py
@@ -1,42 +1,64 @@
 import json
-with open("expences.json","r") as file:
-    expences=json.load(file)
-print(expences)
-print("-"*5,"Expense Tracker","-"*5)
-expence=[]
+print("-"*8,"Expense Tracker","-"*8)
+with open("expenses.json","r") as file:
+    expense=json.load(file)
 while True:
-    print("1. Add Expense")
-    print("2. View Expenses")
-    print("3. View Total Spent")
-    print("4. Exit")
+    print(f"1. Add Expense")
+    print(f"2. View Expenses")
+    print(f"3. View Total Spent")
+    print(f"4. Delete Expense")
+    print(f"5. Clear Expenses")
+    print(f"6. Exit")
     option=int(input("Choose Option: "))
     print("-"*20)
     
     if option==1:
-        catagory=input("Enter category: ")
-        amount=int(input("Enter amount: "))
-        date=input("Enter date: ")
-        expence.append({"Catagory":catagory,"Amount":amount,"Date":date})
-        print("Expense added successfully!")
+        category=input(f"Enter category: ")
+        amount=int(input(f"Enter amount: "))
+        date=input(f"Enter date: ")
+        expense.append({"Category":category,"Amount":amount,"Date":date})
+        with open("expenses.json","w") as file:
+            json.dump(expense,file)
+        print(f"Expense added successfully!")
         print("-"*20)
     elif option==2:
-        for el in expence:
-            for key,value in el.items():
-                print(f"{key}:{value}")
-            print("-"*20)
+        index=0
+        print(f"{'Category':<15}{'Amount':<10}{'Date':<12}")
+        print("."*36)
+        while index<len(expense):
+            print(f"{index}. {expense[index]["Category"]:<12}{expense[index]["Amount"]:<10}{expense[index]["Date"]:<12}")
+            index+=1
+        print("-"*20)    
     elif option==3:
-        for element in expence:
-            for key,value in element.items():
-                if value==amount:    
-                    total_spent=0
-                    for amount_value in element.items():
-                        total_spent+=amount
-        print("Total spent: ",total_spent)
+        total_spent=0
+        index=0
+        while index<len(expense):
+            total_spent=total_spent+expense[index]["Amount"]
+            index+=1
+        print(f"Total spent: {total_spent}")
         print("-"*20)
     elif option==4:
-        print("Program exited successfully!")
+        index=0
+        print(f"{'Category':<15}{'Amount':<10}{'Date':<12}")
+        print("."*36)
+        while index<len(expense):
+            print(f"{index}. {expense[index]["Category"]:<12}{expense[index]["Amount"]:<10}{expense[index]["Date"]:<12}")
+            index+=1
+        print("-"*20)
+        delete_index=int(input("Enter number to delete: "))
+        expense.pop(delete_index)
+        print("Expense deleted successfully!")
+        print("-"*20)
+    elif option==5:
+        with open("expenses.json","w") as file:
+            json.dump([],file)
+            expense=[]
+        print(f"Expenses cleared successfully!")
+        print("-"*20)    
+    elif option==6:
+        print(f"Program exited successfully!")
         print("-"*20)
         break
     else:
-        print("Enter correct option.")
+        print(f"Please enter correct option.")
         print("-"*20)
